393e8b5dfeb46987026d6e42059082ec52c36f40
Gianluca Minoprio <gianlucaminoprio@icloud.com>
2025-03-30 12:37:38 -0700

    update favicon

--- src/app/meta.ts
+++ src/app/meta.ts
@@ -8,18 +8,40 @@ export const metaOpenGraph = {
     images: ["/og-preview.png"],
   },
   icons: [
-    { rel: "icon", url: "/favicon.png" }
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+      sizes: "any",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      url: "/apple-touch-icon.png",
+    },
   ],
   openGraph: {
     title: "Real World Ethereum",
     description: "Buy a sweet hat with stablecoins.",
     url: "https://daimoshop.vercel.app",
     siteName: "Real World Ethereum",
-    images: [{
-      url: "/og-preview.png",
-      width: 768,
-      height: 512,
-      alt: "Real World Ethereum",
-    }],
+    images: [
+      {
+        url: "/og-preview.png",
+        width: 768,
+        height: 512,
+        alt: "Real World Ethereum",
+      },
+    ],
   },
 };
