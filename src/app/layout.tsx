c055ee95c5d91bb7229fca996e20a9a6a428078c
Gianluca Minoprio <gianlucaminoprio@icloud.com>
2025-05-01 19:10:11 -0700

    update manifest in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content={`${APP_URL}/og.png`} />
+        <meta property="fc:frame:button:1" content="Get Your Cap @ Farcon" />
+        <meta property="fc:frame:post_url" content={`${APP_URL}/api/frame`} />
+        <meta property="fc:frame:embed" content="true" />
+        <meta property="fc:frame:embed:url" content={APP_URL} />
+        <meta property="fc:frame:embed:allowed_hosts" content="daimo-store.vercel.app" />
+        <meta property="fc:frame:embed:aspect_ratio" content="1.91:1" />
+        <meta property="og:image" content={`${APP_URL}/og.png`} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:title" content="Daimo Store" />
+        <meta property="og:description" content="Buy a cap with Digital Money" />
+        <meta property="og:url" content={APP_URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Daimo Store" />
+        <meta name="application-name" content="Daimo Store" />
+        <meta name="theme-color" content="#F1D78B" />
+      </head>
       <body className={`antialiased overscroll-none ${poppins.className}`}>
         <Providers>{children}</Providers>
       </body>
